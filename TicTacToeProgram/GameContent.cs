6abe4f522913cea5859b55a197f1cac8d6c880df
Przemysław Nitoń <97357168+PrzemekNiton@users.noreply.github.com>
2022-06-27 09:41:06 +0200

    Add files via upload

---
+++ TicTacToeProgram/GameContent.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    internal class GameContent
+    {
+        public string actualPlayer { get; set; }
+
+        private const string X = "X";
+        private const string O = "O";
+
+        public void SetNextUser()
+        {
+            if (actualPlayer == X)
+            {
+                actualPlayer = O;
+                
+            }
+            else
+            {
+                actualPlayer = X;
+            }
+
+        }
+
+      
+
+    }
+}
---
+++ TicTacToeProgram/MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TicTacToe
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        GameContent _gameContent = new GameContent();
+        private Button control;
+
+        // button user 
+        private void UserClickArea(object sender, RoutedEventArgs e)
+        {
+            var SpaceArea = (Button)sender;
+            if (!String.IsNullOrWhiteSpace(SpaceArea.Content?.ToString()))
+            {
+              
+                return;
+                
+            }
+            SpaceArea.Content = _gameContent.actualPlayer;
+
+            _gameContent.SetNextUser();
+          
+
+        }
+
+        // button event click 
+        // Reset Games
+        private void restartnewgame_Click(object sender, RoutedEventArgs e)
+        {
+            foreach(var control in boardGrid.Children)
+                {
+                if(control is Button)
+                {
+                    ((Button)control).Content = String.Empty;
+
+
+                }
+                else if (control is Button)
+                {
+
+                    MessageBox.Show("The games is end");
+                }
+
+
+            }
+            _gameContent = new GameContent();
+        }
+
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
+            
+            }
+
+        }
+    }
+
